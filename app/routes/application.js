5c4d741da7a54ff12e6bfd401ecc48d176531fb0
kculafic <kculafic@gmail.com>
2017-04-25 11:06:18 -0700

    Bumbox rename

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
+// ******** should perhaps be 'app/models/album-fixtures'
+import albums from 'app/models/album-fixtures';
+
 export default Ember.Route.extend({
   model() {
+    return albums;
   }
 });
