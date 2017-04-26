756f623b56ba498ade4ff1d8bcd7bbac31858aec
kculafic <kculafic@gmail.com>
2017-04-26 14:32:07 -0700

    Complete Data MOdels, pass all tests

--- app/routes/album.js
+++ app/routes/album.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
-import albums from '../models/album-fixtures';
+// import albums from '../models/album-fixtures';
 
 export default Ember.Route.extend({
   model: function(params) {
+
+    console.log(this.store.find('album', params.album_id));
     return this.store.find('album', params.album_id)
   }
 });
