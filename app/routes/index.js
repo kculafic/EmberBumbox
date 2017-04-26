43232eda1aa0e7221c975b4dc45bddd95e9bf1bf
kculafic <kculafic@gmail.com>
2017-04-26 14:39:16 -0700

    pre-merge

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
-// import albums from '../models/album-fixtures';
 
 export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('album');
   }
 });
-
-
-// export default Ember.Route.extend({
-//   model: function() {
-//     return albums;
-//   }
-// });
