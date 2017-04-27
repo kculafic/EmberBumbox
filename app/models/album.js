0fd71a4bedcdae12a2e624e10bc9e70343c497eb
kculafic <kculafic@gmail.com>
2017-04-27 15:53:40 -0700

    Complete computed properties, passing tests - 08

--- app/models/album.js
+++ app/models/album.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 var attr = DS.attr;
 var hasMany = DS.hasMany;
+var computed = Ember.computed;
 
 export default DS.Model.extend({
   artwork: attr('string'),
@@ -9,11 +10,8 @@ export default DS.Model.extend({
   isExplicit: attr('boolean'),
   songs: hasMany('song'),
 
-  totalDuration: function() {
-    var total = 0;
-    this.get('songs').forEach(function(song) {
-      total += song.get('duration');
-    });
-    return total;
-  }.property('songs.@each.duration')
+  songDurations: computed.mapBy('songs', 'duration'),
+  totalDuration: computed.sum('songDurations'),
+  songCount: computed.alias('songs.length')
+  
 });
