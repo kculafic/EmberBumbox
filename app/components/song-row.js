25d23632c60853519fe22fe2e0b4f752c9f7d8a0
kculafic <kculafic@gmail.com>
2017-04-28 15:40:43 -0700

    Complete step 11, pass tests

--- app/components/song-row.js
+++ app/components/song-row.js
@@ -1,16 +1,21 @@
 import Ember from 'ember';
+var inject = Ember.inject;
 
 export default Ember.Component.extend({
   tagName: 'tr',
   isPlaying: false,
 
+  player: inject.service(),
+
   actions: {
     play: function() {
+      this.get('player').play(this.get('song'));
       this.set('isPlaying', true);
     },
 
     pause: function() {
-      this.set('isPlaying', false); 
+      this.get('player').pause(); 
+      this.set('isPlaying', false);
     }
   }
 });
