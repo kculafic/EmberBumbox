bdba8643ed5a992a937aad4dfbaf6c69c75b31c1
kculafic <kculafic@gmail.com>
2017-05-12 15:30:45 -0700

    Friday

--- app/routes/album.js
+++ app/routes/album.js
@@ -5,7 +5,6 @@ export default Ember.Route.extend({
   model: function(params) {
 
     console.log(this.store.find('album', params.album_id));
-    return this.store.find('album', params.album_id); 
-
+    return this.store.find('album', params.album_id);
   }
 });
